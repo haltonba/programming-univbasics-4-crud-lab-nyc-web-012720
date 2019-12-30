67c6e57eba37eb1b262d7b7f20d64e330eb58590
Halton Bagley <haltonba94@gmail.com>
2019-12-30 17:21:35 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,39 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  four_elements = ["One", "Two", "Three", "Four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  adding_end = array.push element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  adding_beginning = array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_end = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_beginning = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
 end
